f85eed63223d7ec5d7972a456795bee48d25752a
olgaIsakov <olgaisakov821997?>
2021-01-09 18:33:11 +0200

    methos call changes

--- src/methods/CheckSingleMethod.java
+++ src/methods/CheckSingleMethod.java
@@ -10,19 +10,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CheckSingleMethod {
+    private static final String START_PARAMETERS = "(";
     List<String> globalVars;
     private final static String IF_WHILE = ".*\\s*if|while\\s*\\(\\s*.*\\s*\\)\\s*\\{\\s*.*\\s*\\}$";
+    private final static String METHOD_CALL = "[a-zA-Z]\\w*\\s*\\(\\s*.*\\s*\\)\\s*;\\s*$";
+    private final static String SPACES = "\\s+";
+    final static String EMPTY_SPACE = " ";
+    final static int FIRST = 0;
+    final static int SECOND = 1;
+    final static Pattern METHOD_CALL_PATTERN = Pattern.compile(METHOD_CALL);
     final static Pattern IF_WHILE_PATTERN = Pattern.compile(IF_WHILE);
 
-    public void checkMethods(Map<List<String>, List<String>> allMethods){
+    public void checkMethods(Map<List<String>, List<String>> allMethods) throws ConditionException {
         globalVars = Parser.getGlobalVars();
         for (List<String> method: allMethods.keySet()){
-            checkSingleMethod(method);
+            String name = mainMethod.getMethodName(method.get(FIRST));
+            checkSingleMethod(method, name);
         }
     }
 
-    private void checkSingleMethod(List<String> method) throws ConditionException {
+    private void checkSingleMethod(List<String> method, String name) throws ConditionException {
         for (int i = 0 ; i < method.size() ; i++){
+            Matcher ifWhileMatch = IF_WHILE_PATTERN.matcher(method.get(i));
             Matcher ifWhileMatch = IF_WHILE_PATTERN.matcher(method.get(i));
             if (ifWhileMatch.matches()){
                 i = findAllBlocks(method, i);
